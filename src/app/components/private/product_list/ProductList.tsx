a86c3699a8dca793e9aaab113cb2f2bc7da07a0b
shane <shanejohn37@gmail.com>
2024-02-10 21:11:00 -0500

    Intial file structure changes

---
+++ src/app/components/private/product_list/ProductList.tsx
@@ -0,0 +1,25 @@
+// import { ProductType } from "@/app/types/types";
+// import React from "react";
+// import ProductCard from "../product_card/ProductCard";
+
+// type ProductListProps = ProductType[];
+
+// export const ProductList = (props: any) => {
+//   console.log({ props });
+
+//   const { productList } = props;
+
+//   return (
+//     <>
+//       {productList.forEach((productDetails) => {
+//         return <ProductCard productDetails={productDetails} />;
+//       })}
+//     </>
+//   );
+// };
+
+// // const mapStateToProps = (state) => ({});
+
+// // const mapDispatchToProps = {};
+
+// // export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
